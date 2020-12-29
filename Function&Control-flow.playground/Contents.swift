01d489cfdcd9d476e321b1aaf4a36aabcc35c9b1
cashew <wcashew@hotmail.com>
2020-12-29 14:24:54 +0800

    update function&control-flow

---
+++ Function&Control-flow.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+func calcBMI(weight:Double, height:Double) -> String {
+	let bmi = round(weight / (pow(height, 2))) //计算 bmi 指数，体重单位为公斤，身高单位为米
+	var message = "" //状态文字提示
+	if bmi >= 40 {
+		message = "肥胖III级（非常严重肥胖）"
+	} else if bmi >= 35 && bmi < 40 {
+		message = "肥胖II级（严重肥胖）"
+	} else if bmi >= 30 && bmi < 35 {
+		message = "肥胖I级（中等肥胖）"
+	} else if bmi >= 25 && bmi < 30 {
+		message = "体重过重"
+	} else if bmi >= 18.5 && bmi < 25 {
+		message = "体重正常（健康体重）"
+	} else if bmi >= 16 && bmi < 18.5 {
+		message = "体重过轻"
+	} else if bmi >= 15 && bmi < 16 {
+		message = "严重体重不足"
+	} else {
+		message = "非常严重的体重不足"
+	}
+	return "你的 BMI 是\(bmi)，状态是\(message)。"
+}
+print(calcBMI(weight: 70, height: 1.70))
