854686eeac3cc53738e9f98f274c9d0f1c61d620
cashew <wcashew@hotmail.com>
2020-12-31 12:28:32 +0800

    update APP calculation

--- BMICalculator/BMICalculator/ViewController.swift
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	
+	@IBOutlet weak var displayer: UILabel!
+	@IBOutlet weak var heightInput: UITextField!
+	@IBOutlet weak var weightInput: UITextField!
+	
+	var height: Double?
+	var weight: Double?
+	
+	@IBAction func calculation(_ sender: UIButton) {
+		
+		height = Double(heightInput.text!)
+		weight = Double(weightInput.text!)
+		
+		guard let height = height else {
+			displayer.text = "输入错误，请重新输入！"
+			return
+		}
+		guard let weight = weight else {
+			displayer.text = "输入错误，请重新输入！"
+			return
+		}
+		displayer.text = BMICalculator(height: height, weight: weight)
 	}
-
-
+	
+	func BMICalculator(height: Double, weight: Double) -> String {
+		
+		let BMI = weight / (height * height)
+		let textValue = String(format: "%2.f", BMI)
+		
+		switch BMI {
+			case 0.0..<18.5:
+				return "您的BMI指数为\(textValue)，状态为过轻"
+			case 18.5..<24.0:
+				return "您的BMI指数为\(textValue)，状态为正常"
+			case 24.0..<28.0:
+				return "您的BMI指数为\(textValue)，状态为过重"
+			case 28.0...:
+				return "您的BMI指数为\(textValue)，状态为肥胖"
+			default:
+				return "输入错误，请重新输入！"
+		}
+	}
+	
 }
 
