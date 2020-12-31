88c14c253eea122b2978f46efbc3f6fc346442c1
cashew <wcashew@hotmail.com>
2020-12-31 12:26:07 +0800

    create a project

---
+++ BMICalculator/BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by cashew on 2020/12/30.
+//  Copyright © 2020 cashew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
